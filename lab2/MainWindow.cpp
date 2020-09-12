c836e4357f39353675a247bc5d08b013c4c9aa0e
Megan Dorian <mishakollins68@gmail.com>
2020-09-12 18:23:15 +0300

    feat: Добавлен интерфейс

--- lab2/MainWindow.cpp
+++ lab2/MainWindow.cpp
@@ -1,13 +1,45 @@
 #include <windows.h>
 
 LRESULT CALLBACK WindowProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    HDC hdc;
+    HINSTANCE hInst;
+    HFONT font;
+    HWND powerButton, workButton, modeButton;
+    PAINTSTRUCT ps;
+    RECT window;
     switch (uMsg) {
-        case WM_CLOSE:
-            exit(0);
+        case WM_CREATE:
+            hInst = ((LPCREATESTRUCT)lParam)->hInstance;
+            powerButton = CreateWindow(L"button", L"выключено",
+                                WS_CHILD | WS_VISIBLE | WS_BORDER,
+                                120, 85, 85, 30, hWnd, 0, hInst, NULL);
+            ShowWindow(powerButton, SW_SHOWNORMAL);
+            workButton = CreateWindow(L"button", L"+1 мин",
+                                WS_CHILD | WS_VISIBLE | WS_BORDER,
+                                80, 140, 75, 30, hWnd, 0, hInst, NULL);
+            ShowWindow(powerButton, SW_SHOWNORMAL);
+            workButton = CreateWindow(L"button", L"холодный",
+                                WS_CHILD | WS_VISIBLE | WS_BORDER,
+                                110, 195, 90, 30, hWnd, 0, hInst, NULL);
+            ShowWindow(powerButton, SW_SHOWNORMAL);
             break;
-        default:
-            return DefWindowProc(hWnd, uMsg, wParam, lParam);
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            SetRect(&window, 10, 10, 230, 60);
+            FillRect(hdc, &window, (HBRUSH)BLACK_BRUSH);
+
+            font = CreateFont(22, 9, 0, 0, FW_NORMAL, false, false, false, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, L"Arial");
+            SelectObject(hdc, font);
+            TextOut(hdc, 30, 90, L"Питание:", 8);
+            TextOut(hdc, 30, 200, L"Режим:", 6);
+            EndPaint(hWnd, &ps);
+            DeleteObject(font);
+            break;
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            break;
     }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
 void checkIsCLassRegistered (WNDCLASSEX windowClass) {
@@ -30,14 +62,14 @@ int WINAPI WinMain(HINSTANCE hInst,
                    int nCommandShow) {
     int screenWidth = GetSystemMetrics(SM_CXSCREEN),
         screenHeight = GetSystemMetrics(SM_CYSCREEN),
-        mainWindowWidth = 500,
-        mainWindowHeight = 250;
+        mainWindowWidth = 250,
+        mainWindowHeight = 300;
     MSG uMsg;
     WNDCLASSEX mainWindowClass;
     HWND mainWindow;
     memset(&mainWindowClass, 0, sizeof(WNDCLASSEX));
     mainWindowClass.cbSize = sizeof(WNDCLASSEX);
-    mainWindowClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    mainWindowClass.hbrBackground = (HBRUSH)WHITE_BRUSH;
     mainWindowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
     mainWindowClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     mainWindowClass.hInstance = hInst;
@@ -46,7 +78,9 @@ int WINAPI WinMain(HINSTANCE hInst,
 
     checkIsCLassRegistered(mainWindowClass);
 
-    mainWindow = CreateWindow(mainWindowClass.lpszClassName, L"Кондиционер", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+    //mainWindow = CreateWindow(mainWindowClass.lpszClassName, L"Пульт", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+                              //(screenWidth - mainWindowWidth) / 2, (screenHeight - mainWindowHeight) / 2, mainWindowWidth, mainWindowHeight, NULL, NULL, hInst, NULL);
+    mainWindow = CreateWindow(mainWindowClass.lpszClassName, L"Пульт", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX,
                               (screenWidth - mainWindowWidth) / 2, (screenHeight - mainWindowHeight) / 2, mainWindowWidth, mainWindowHeight, NULL, NULL, hInst, NULL);
 
     checkIsWindowCreated(mainWindow);
